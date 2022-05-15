3ae68279be69f976360f67459c82bd039b533f37
Roma2351 <95650797+Roma2351@users.noreply.github.com>
2022-05-16 00:02:33 +0400

    complete

--- src/index.js
+++ src/index.js
@@ -23,12 +23,26 @@ function renderItem({ name, price, count }) {
     // добавляет на страницу верстку для одного продукта
 
     addHtml(`
-    <p>
-      ${name} [ ${price} руб ] - ${count} шт
-      <button class="addTocart" data-name="${name}" data-price="${price}">
-        Добавить
-      </button>
+    <div class="p-3 mb-2 bg-secondary text-white">
+
+    <p class="text-center fw-bold">
+
+    <img src="image1.png">${name}</img>[ ${price} руб <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
+    <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
+    <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2H3z"/>
+    </svg> ] - ${count} шт
+
+    <button class="addTocart btn btn-warning" data-name="${name}" data-price="${price}">
+    Добавить
+    </button>
+
+    <button class="removeTocart btn btn-danger" data-name="${name}" data-price="${price}">
+    Удалить
+    </button>
+
     </p>
+
+    </div>
   `);
 }
 
@@ -42,7 +56,16 @@ function renderPage() {
         renderItem(cart[product.name]);
     }
     // добавляем в конце итог
-    addHtml(`Итог: ${totalPrice} руб`);
+    
+    addHtml(`<div class="p-3 mb-2 bg-success text-white">
+
+    <p class="text-center fw-bold">
+
+      Итог : ${totalPrice} <img src="rubl.png"></img>
+    </p>
+
+    </div>
+    `);
 
     // выбирает все кнопки по классу addTocart
     document.querySelectorAll(".addTocart").forEach((el) => {
@@ -60,6 +83,24 @@ function renderPage() {
             // перерисовываем страницу
             renderPage();
         });
+    });
+
+    document.querySelectorAll(".removeTocart").forEach((el) => {
+      el.addEventListener("click", (event) => {
+        let name = event.target.getAttribute("data-name");
+        let price = event.target.getAttribute("data-price");
+  
+        totalPrice -= price;
+        if (totalPrice < 0) {
+          totalPrice = 0;
+        }
+  
+        cart[name].count -= 1;
+        if (cart[name].count < 0) {
+          cart[name].count = 0;
+        }
+        renderPage();
+      });
     });
 }
 
